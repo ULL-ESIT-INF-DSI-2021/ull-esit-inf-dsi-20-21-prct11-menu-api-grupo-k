4b06ddd653e7349b3edd0a0bf44b5d09a7e3f9d4
Elvis Nogueiras <alu0101281308@ull.edu.es>
2021-05-20 23:32:36 +0100

    Estructura nueva para la practica menu

---
+++ src/menu/personalized_menu.ts
@@ -0,0 +1,20 @@
+import { Plate } from "../plate/plate";
+import { Menu } from "./menu";
+
+export class PerzonalisedMenu extends Menu {
+    
+    constructor(name: string) {
+      super(name);
+      
+    }
+
+    setMenu(plates: Plate[]): Boolean {
+    this.plates = plates;
+    this.calculatePrice();
+    this.calculateNutritionalComposition();
+    this.identifyAlimentGroupList();
+    this.verify_menu = true;
+    return true;
+    }
+  
+}
