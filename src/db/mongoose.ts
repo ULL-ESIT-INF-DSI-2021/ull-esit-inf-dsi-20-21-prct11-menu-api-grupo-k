0b6d4d62e1161864f27ea49598ada6eb736f5dac
Daniel Fuentes <alu0101166247@ull.edu.es>
2021-05-21 23:24:32 +0100

    18

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -6,7 +6,6 @@ const mongodb_url = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/menu-a
 mongoose.connect(mongodb_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false,
 }).then(() => {
   console.log('Connection to MongoDB server established');
--- src/index2.ts
+++ src/index2.ts
@@ -3,7 +3,7 @@
 /* eslint-disable camelcase */
 import {Aliment, AlimentGroup} from './class/aliment/aliment';
 import {Category, Plate} from './class/plate/plate';
-import {plateModel} from './models/plates/PlatesSchema';
+import {plateModel, PlatesSchema} from './models/plates/PlatesSchema';
 import * as mongoose from 'mongoose';
 import './db/mongoose';
 
@@ -13,9 +13,23 @@ const queso_mozarella = new Aliment('Queso mozarella', 1, 21.1, 20, 0, 353, 0, 0
 
 const ingredientes = new Map<Aliment, number>();
 
-const test = new Map<string, number>();
-test.set('pollo', 20);
-// test.set('arroz', 10);
+const test: [aliment: string, quantity: number][] = [];
+test[0] = ['hola', 10];
+test[1] = ['chao', 20];
+
+type predominantType = {
+    alimentGroup: string,
+    quantity: number,
+}
+
+type ingreType = {
+    aliment: string,
+    quantity: number,
+}
+
+const pre: predominantType = {alimentGroup: 'pollito', quantity: 12};
+
+const ing: ingreType[] = [{aliment: 'pollitox', quantity: 112}, {aliment: 'pollito', quantity: 12}];
 
 ingredientes.clear();
 ingredientes.set(queso_mozarella, 20);
@@ -23,11 +37,25 @@ ingredientes.set(queso_mozarella, 20);
 ingredientes.set(queso_gouda, 10);
 const plato_arepitas = new Plate('Arepitas con queso', ingredientes, Category.entree);
 
-console.log(JSON.stringify(test));
 
-console.log(plato_arepitas);
+const plate = mongoose.model('plates', PlatesSchema);
+const item = new plate;
+item.name = 'arepas2';
+item.category = 0;
+item.calories = 0;
+item.protein = 0;
+item.fats = 0;
+item.carbohydrates = 0;
+item.starch = 0;
+item.sugars = 0;
+item.fiber = 0;
+item.water = 0;
+item.price = 0;
+item.ingredients = ing;
+item.predominantAlimentGroup = pre;
 
-const plate = new plateModel({
+
+/* const plate = new plateModel({
   name: 'Arepitas con queso',
   category: '0',
   calories: '0',
@@ -39,19 +67,17 @@ const plate = new plateModel({
   fiber: '0',
   water: '0',
   price: '98.2',
-  predominantAlimentoGroup: {
-    alimentGroup: 'pollo',
-    quantity: '20',
-  },
+  ingredients: test,
 });
-
+*/
 
 const axios = require('axios').default;
 
-axios.post('https://grupo-k-p11-menu-app.herokuapp.com/plates', plate).then(function(response: any) {
+axios.post('https://grupo-k-p11-menu-app.herokuapp.com/plates', item).then(function(response: any) {
   console.log(response.status);
   mongoose.connection.close();
 }).catch(function(error: any) {
   console.log(error);
   mongoose.connection.close();
 });
+
