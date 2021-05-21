3aebc1d0dfa8e02678ee4b50bd0f0224d8b5615e
Daniel Fuentes <alu0101166247@ull.edu.es>
2021-05-21 01:02:13 +0100

    heroku config

---
+++ src/db/mongoose.ts
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+import {connect} from 'mongoose';
+
+const mongodb_url = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/menu-app';
+
+connect(mongodb_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(() => {
+  console.log('Connection to MongoDB server established');
+}).catch(() => {
+  console.log('Unnable to connect to MongoDB server');
+});
